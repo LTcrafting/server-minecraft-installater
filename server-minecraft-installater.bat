fbe1b2c890b93565e57f94625bc9f78726ae8928
LTcrafting <62606601+LTcrafting@users.noreply.github.com>
2021-02-15 15:49:43 +0100

    Add files via upload

---
+++ server-minecraft-installater.bat
@@ -0,0 +1,11 @@
+@echo
+title server-minecraft-installater
+echo "Creation du fichier de lancement du serveur"
+if exist start.bat echo "Le fichier de lancement existe deja (start.bat)" else 
+echo @echo off>start.bat
+echo java -Xms512M -Xmx1024M -jar spigot-1.16.5.jar>>start.bat
+echo pause>>start.bat
+echo "Installation de spigot-1.16.5"
+if not exist spigot-1.16.5.jar curl -s -O https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar else "Spigot deja present"
+echo "Termine, lance start.bat pour lancer ton serveur Minecraft"
+timeout 10
