6f93fa4d965582c51b454c5aef4d86f0d23f9af5
LTcrafting <62606601+LTcrafting@users.noreply.github.com>
2021-02-15 18:26:56 +0100

    Update server-minecraft-installater.bat

--- server-minecraft-installater.bat
+++ server-minecraft-installater.bat
@@ -8,4 +8,4 @@ echo pause>>start.bat
 echo "Installation de spigot-1.16.5"
 if not exist spigot-1.16.5.jar curl -s -O https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar else "Spigot deja present"
 echo "Termine, lance start.bat pour lancer ton serveur Minecraft"
-timeout 10
+timeout 5
